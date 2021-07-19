215da3e8d1829af2e0fcb602ae6def294b0414a2
Bryden Frizzell <bryden.frizzell@gmail.com>
2021-07-19 13:28:05 +1200

    Implemented bilinear interpolated prediction wavelet transform, decent entropy reduction

--- WaveletDecodeLayer.cpp
+++ WaveletDecodeLayer.cpp
@@ -11,35 +11,148 @@ WaveletDecodeLayer::WaveletDecodeLayer(const std::vector<uint16_t>& wavelets, co
     auto currWavelet = wavelets.begin();
 
     // TODO this can be rearranged to run faster
-    for (uint32_t y = 0; y < size.GetHeight(); y += 2)
+    for (int32_t y = 0; y < size.GetHeight(); y += 2)
     {
-        for (uint32_t x = 0; x < size.GetWidth(); x += 2)
+        for (int32_t x = 0; x < size.GetWidth(); x += 2)
         {
-            uint32_t parentX = x / 2;
-            uint32_t parentY = y / 2;
-            // get prediction/parent value
-            uint16_t parent = parentVals[parentY * size.GetParentWidth() + parentX];
-            uint16_t predicted = parent;
+            // Bilinear wavelet
+            // Parent vals:
+            // X O X O
+            // O O O O
+            // X O X O
+            // O O O O
+            // Step 1: Decode diagonals by averaging parents (X pattern, usually 4 vals)
+            // X O X O
+            // O D O D
+            // X O X O
+            // O D O D
+            // Step 2: Decode vertical/horizontal using parents + diag (+ pattern, usually 4 vals)
+            // X D X D
+            // D X D X
+            // X D X D
+            // D X D X
+
+            int32_t parentX = x / 2;
+            int32_t parentY = y / 2;
+
+            // Top left is guaranteed
+            uint16_t TL = parentVals[parentY * size.GetParentWidth() + parentX];
 
             // Parent transform is TL, so no wavelet needed
-            pixelVals[y * size.GetWidth() + x] = predicted;
+            pixelVals[y * size.GetWidth() + x] = TL;
 
-            // add wavelet to get final value
+            // for prediction values shared between outputs
+            // number of values used in prediction
+            uint32_t predictionCountBase = 1;
+            uint32_t predictionBase = TL;
+
+            // DIAGONAL PREDICION - average up to 4 parent values
+            // X-shaped pattern of parent values
+            if (x + 1 < size.GetWidth() && y + 1 < size.GetHeight())
+            {
+                uint32_t predictionCount = predictionCountBase;
+                uint32_t predicted = predictionBase;
+
+                // Add TR parent if possible
+                if (parentX + 1 < size.GetParentWidth())
+                {
+                    predicted += parentVals[parentY * size.GetParentWidth() + parentX + 1];
+                    ++predictionCount;
+                }
+
+                // Add BL parent if possible
+                if (parentY + 1 < size.GetParentHeight())
+                {
+                    predicted += parentVals[(parentY + 1) * size.GetParentWidth() + parentX];
+                    ++predictionCount;
+                    // Add BR parent if possible
+                    if (parentX + 1 < size.GetParentWidth())
+                    {
+                        predicted += parentVals[(parentY + 1) * size.GetParentWidth() + parentX + 1];
+                        ++predictionCount;
+                    }
+                }
+
+                // fix rounding
+                // 1 = +0
+                // 2-3 = +1
+                // 4 = +2
+                predicted += predictionCount / 2;
+
+                // average
+                predicted = predicted / predictionCount;
+
+                // add wavelet to get final value
+                uint16_t outputVal = predicted + *currWavelet;
+                pixelVals[(y + 1) * size.GetWidth() + x + 1] = outputVal;
+                ++currWavelet;
+
+                // Diag is used as input to other output predictions
+                predictionBase += outputVal;
+                predictionCountBase += 1;
+            }
+            
+            // TOP RIGHT prediction - uses output of previous block decodes + parent vals + freshly-decoded diagonal
+            // predictionBase contains this nodes left + bottom, need to add top + right
+            // +-shaped pattern of parent values
             if (x + 1 < size.GetWidth())
             {
+                uint32_t predictionCount = predictionCountBase;
+                uint32_t predicted = predictionBase;
+
+                // Add right parent if possible
+                if (parentX + 1 < size.GetParentWidth())
+                {
+                    predicted += parentVals[parentY * size.GetParentWidth() + parentX + 1];
+                    ++predictionCount;
+                }
+
+                // Add top (diag of above block) if possible
+                if (y - 1 > 0)
+                {
+                    predicted += pixelVals[(y - 1) * size.GetHeight() + x + 1];
+                    ++predictionCount;
+                }
+
+                // fix rounding
+                predicted += predictionCount / 2;
+
+                // average
+                predicted = predicted / predictionCount;
+
                 pixelVals[y * size.GetWidth() + x + 1] = predicted + *currWavelet;
                 ++currWavelet;
             }
 
+            // BOTTOM-LEFT prediction - uses output of previous block decodes + parent vals + freshly-decoded diagonal
+            // predictionBase contains this nodes top + right, need to add left + bottom
+            // +-shaped pattern of parent values
             if (y + 1 < size.GetHeight())
             {
-                pixelVals[(y + 1) * size.GetWidth() + x] = predicted + *currWavelet;
-                ++currWavelet;
-            }
+                uint32_t predictionCount = predictionCountBase;
+                uint32_t predicted = predictionBase;
 
-            if (x + 1 < size.GetWidth() && y + 1 < size.GetHeight())
-            {
-                pixelVals[(y + 1) * size.GetWidth() + x + 1] = predicted + *currWavelet;
+                // Add bottom parent if possible
+                if (parentY + 1 < size.GetParentHeight())
+                {
+                    predicted += parentVals[(parentY + 1) * size.GetParentWidth() + parentX];
+                    ++predictionCount;
+                }
+
+                // Add left (diag of previous block) if possible
+                if (x - 1 > 0)
+                {
+                    predicted += pixelVals[(y + 1) * size.GetWidth() + (x - 1)];
+                    ++predictionCount;
+                }
+
+                // fix rounding
+                predicted += predictionCount / 2;
+
+                // average
+                predicted = predicted / predictionCount;
+
+                pixelVals[(y + 1) * size.GetWidth() + x] = predicted + *currWavelet;
                 ++currWavelet;
             }
         }
--- WaveletEncodeLayer.cpp
+++ WaveletEncodeLayer.cpp
@@ -15,32 +15,153 @@ WaveletEncodeLayer::WaveletEncodeLayer(std::vector<uint16_t> values, uint32_t wi
 
     auto currWavelet = wavelets.begin();
 
-    for (uint32_t y = 0; y < height; y += 2)
+    for (int32_t y = 0; y < height; y += 2)
     {
-        for (uint32_t x = 0; x < width; x += 2)
+        for (int32_t x = 0; x < width; x += 2)
         {
-            // set parent value
-            uint32_t parentX = x / 2;
-            uint32_t parentY = y / 2;
-            uint16_t parent = values[y * width + x];
-            uint16_t predicted = values[y * width + x];
-            parentVals[parentY * size.GetParentWidth() + parentX] = parent;
+            // Bilinear wavelet
+            // Parent vals:
+            // X O X O
+            // O O O O
+            // X O X O
+            // O O O O
+            // Step 1: Encode diagonals by averaging parents (X pattern, usually 4 vals)
+            // X O X O
+            // O D O D
+            // X O X O
+            // O D O D
+            // Step 2: Encode vertical/horizontal using parents + diag (+ pattern, usually 4 vals)
+            // X D X D
+            // D X D X
+            // X D X D
+            // D X D X
+            
+            int32_t parentX = x / 2;
+            int32_t parentY = y / 2;
 
-            if (x + 1 < width)
-            {
-                *currWavelet = values[y * width + x + 1] - predicted;
-                ++currWavelet;
-            }
+            // Top left is guaranteed
+            uint16_t TL = values[y * width + x];
 
-            if (y + 1 < height)
-            {
-                *currWavelet = values[(y + 1) * width + x] - predicted;
-                ++currWavelet;
-            }
+            // Parent transform is TL, so no wavelet needed
+            parentVals[parentY * size.GetParentWidth() + parentX] = TL;
 
+            // Diagonal gets decoded first
+            // X-shaped averaging
             if (x + 1 < width && y + 1 < height)
             {
-                *currWavelet = values[(y + 1) * width + x + 1] - predicted;
+                // TL
+                uint32_t prediction = TL;
+                uint32_t predictionCount = 1;
+
+                // TR
+                if (x + 2 < width)
+                {
+                    prediction += values[y * width + x + 2];
+                    ++predictionCount;
+                }
+
+                // BL
+                if (y + 2 < height)
+                {
+                    prediction += values[(y + 2) * width + x];
+                    ++predictionCount;
+                }
+
+                // BR
+                if (x + 2 < width && y + 2 < height)
+                {
+                    prediction += values[(y + 2) * width + x + 2];
+                    ++predictionCount;
+                }
+
+                // fix rounding
+                // 1 = +0
+                // 2-3 = +1
+                // 4 = +2
+                prediction += predictionCount / 2;
+
+                // average
+                prediction = prediction / predictionCount;
+
+                *currWavelet = values[(y + 1) * width + x + 1] - prediction;
+                ++currWavelet;
+            }
+
+            // simplified from decoder since we have all the leaf data
+            // +-shaped averaging
+            if (x + 1 < width)
+            {
+                // Left
+                uint32_t prediction = TL;
+                uint32_t predictionCount = 1;
+
+                // Right
+                if (x + 2 < width)
+                {
+                    prediction += values[y * width + x + 2];
+                    ++predictionCount;
+                }
+
+                // Top
+                if (y - 1 > 0)
+                {
+                    prediction += values[(y - 1) * width + x + 1];
+                    ++predictionCount;
+                }
+
+                // Bottom
+                if (y + 1 < height)
+                {
+                    prediction += values[(y + 1) * width + x + 1];
+                    ++predictionCount;
+                }
+
+                // fix rounding
+                prediction += predictionCount / 2;
+
+                // average
+                prediction = prediction / predictionCount;
+
+                *currWavelet = values[y * width + x + 1] - prediction;
+                ++currWavelet;
+            }
+
+            // simplified from decoder since we have all the leaf data
+            // +-shaped averaging
+            if (y + 1 < height)
+            {
+                // Top
+                uint32_t prediction = TL;
+                uint32_t predictionCount = 1;
+
+                // Bottom
+                if (y + 2 < height)
+                {
+                    prediction += values[(y + 2) * width + x];
+                    ++predictionCount;
+                }
+
+                // Left
+                if (x - 1 > 0)
+                {
+                    prediction += values[(y + 1) * width + (x - 1)];
+                    ++predictionCount;
+                }
+
+                // right
+                if (x + 1 < width)
+                {
+                    prediction += values[(y + 1) * width + x + 1];
+                    ++predictionCount;
+                }
+
+                // fix rounding
+                prediction += predictionCount / 2;
+
+                // average
+                prediction = prediction / predictionCount;
+
+                *currWavelet = values[(y + 1) * width + x] - prediction;
                 ++currWavelet;
             }
         }
